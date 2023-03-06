2236cd8cc01a66587b8be3de3f43257c418b36a0
jstevenson02 <jacobryanstevenson@gmail.com>
2023-03-06 17:03:55 -0600

    Nav bar and Hero finished!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Jacob Stevenson &nbsp;
-            <span className="sm:block hidden">| Dev For Hire</span>
+            Jacob&nbsp;
+            <span className="sm:block hidden">| Open For Work ✅</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10 ml-auto">
