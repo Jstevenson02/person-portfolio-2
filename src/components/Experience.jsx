9afb5ddd169c4c456c15c780036d85e67e72fa88
jstevenson02 <jacobryanstevenson@gmail.com>
2023-03-07 11:40:31 -0600

    Hero Finished, SectionWrapper added, Navbutton Add

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
 
 const Experience = () => {
-  return (
-    <div>Experience</div>
-  )
-}
+  return <div>Experience</div>;
+};
 
-export default Experience
+export default Experience;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,17 @@ const Hero = () => {
             className="flex justify-center items-start w-[35px] h-[64px] 
             rounded-3xl border-4 border-secondary p-2"
           >
-            <motion.dev animate />
+            <motion.dev
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
           </div>
         </a>
       </div>
