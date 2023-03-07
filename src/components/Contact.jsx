464f7d06b453002e4af6882242d68986ca42450c
jstevenson02 <jacobryanstevenson@gmail.com>
2023-03-07 14:29:46 -0600

    Implemented Contact form functionality!

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,50 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "Jacob_Stevenson",
+        "template_2ika1ta",
+        {
+          from_name: form.name,
+          to_name: "Jacob",
+          from_email: form.email,
+          to_email: "jacobryanstevenson@gmail.com",
+          message: form.message,
+        },
+        "eFJiQ5W2ozsXCGYLp"
+      )
+      .then(
+        () => {
+          setLoading(false);
+
+          alert("Thank you! I will get back to you as soon as possible!");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.log(error);
+
+          alert("Oops. Something went wrong, try again later.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
