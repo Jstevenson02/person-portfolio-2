5c8d03d5f062b3e40e2e5b3adead64a98589dfaf
jstevenson02 <jacobryanstevenson@gmail.com>
2023-03-09 22:55:17 -0600

    About page updated!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,8 +47,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I am a self-teaching, fulltime student currently pursuing my Associates
-        of Applied Science. I run a side gig creating websites for small
+        I am a self-taught, fulltime student currently pursuing my Associates of
+        Applied Science. I run a side gig creating websites for small
         businesses. I am always expanding my toolkit and have knack for problem
         solving!
       </motion.p>
